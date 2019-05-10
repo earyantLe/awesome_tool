452aad51e7833c73b33e039cfb7a627eb2beb3ff
lirui23 <lirui23@baidu.com>
2019-05-10 10:36:33 +0800

    init

---
+++ 37/sys_manager/fabfile.py
@@ -0,0 +1,14 @@
+# -*- coding: UTF-8 -*-
+#
+# Copyright (c) 2019 Baidu.com, Inc. All Rights Reserved
+#
+"""
+
+
+Authors: lirui23(lirui23@baidu.com)
+Date:    2019-05-10 08:25
+"""
+
+
+def hello():
+    print("Hello world!")
